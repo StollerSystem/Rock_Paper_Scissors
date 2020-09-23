c06584e82495f4a95b0824610a155e52236e5f20
Ben Stoller <stollersystemsound@gmail.com>
2020-09-23 13:53:12 -0700

    refactor logic 2nd time
    co-authored-by: William Donovan-Seid <wdstwo@live.com>

--- Rock_Paper_Scissors/Models/Rock_Paper_Scissors.cs
+++ Rock_Paper_Scissors/Models/Rock_Paper_Scissors.cs
@@ -4,39 +4,19 @@ namespace RockPaperScissors
   {
     public static string Play (string p1Hand, string p2Hand)
     {
-      if (p1Hand == p2Hand)
-      {
-        return "draw";
-      } 
       // PLAYER 1
-      else if (p1Hand == "rock" && p2Hand == "scissors")
+      if (p1Hand == "rock" && p2Hand == "scissors" || p1Hand == "scissors" && p2Hand == "paper" || p1Hand == "paper" && p2Hand == "rock")
       {
         return "Player One Wins";
       } 
-      else if (p1Hand == "scissors" && p2Hand == "paper")
-      {
-        return "Player One Wins";
-      }  
-      else if (p1Hand == "paper" && p2Hand == "rock")
-      {
-        return "Player One Wins";
-      }  
       // PLAYER 2
-      else if (p1Hand == "rock" && p2Hand == "paper")
+      else if (p1Hand == "rock" && p2Hand == "paper" || p1Hand == "scissors" && p2Hand == "rock" || p1Hand == "paper" && p2Hand == "scissors")
       {
         return "Player Two Wins";
-      } 
-      else if (p1Hand == "scissors" && p2Hand == "rock")
-      {
-        return "Player Two Wins";
-      }  
-      else if (p1Hand == "paper" && p2Hand == "scissors")
-      {
-        return "Player Two Wins";
-      }              
+      }       
       else
       {
-        return "whatever";
+        return "draw";
       }
     }
   }
